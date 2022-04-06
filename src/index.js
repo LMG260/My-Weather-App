25292a7ec46150f58d571a071131bd510acca951
LMG260 <102467793+LMG260@users.noreply.github.com>
2022-04-06 23:39:45 +0100

    Added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -53,11 +53,15 @@ function searchCityName(event) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind-speed");
+    let iconElement = document.querySelector("#icon");
     cityElement.innerHTML = cityName;
     tempElement.innerHTML = `${currentTemp}°C`;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    
   }
   axios.get(apiUrl).then(showTemp);
 }
